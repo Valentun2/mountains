7e346431c8ac9f8beb80a2b4d0ef76f4811d2f6b
Valentun2 <pocomaxawrkt@gmail.com>
2023-10-08 16:17:16 +0300

    update

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 
 
 const parallax =document.querySelector(".parallax")
-
-window.onload = function (){
-    if(parallax){
-        
-        const content = document.querySelector('.parallax__container')
-        const clouds=document.querySelector('.images-parallax__clouds')
-        const mountains = document.querySelector('.images-parallax__mountains')
-        const human = document.querySelector('.images-parallax__human')
+const content = document.querySelector('.parallax__container')
+const clouds=document.querySelector('.images-parallax__clouds')
+const mountains = document.querySelector('.images-parallax__mountains')
+const human = document.querySelector('.images-parallax__human')
 
 const forClouds = 40;
 const forMountains = 20;
@@ -18,6 +14,29 @@ const speed = 0.05
 
 let positsonX = 0, positsonY = 0;
 let coordXprocent = 0, coordYprocent = 0; 
+
+window.onload = function (){
+    if(parallax){
+setMouseParallaxStyle()
+
+parallax.addEventListener("mousemove", hadleMouseMove)
+
+let thresholdSets = [];
+for (let i = 0; i <=1; i+=0.005) {
+    thresholdSets.push(i)
+    
+}
+
+const observer = new IntersectionObserver(setParallaxItemsStyle, {
+    threshold: thresholdSets
+});
+
+observer.observe(document.querySelector('.content'));
+
+
+}
+
+
 
 function setMouseParallaxStyle(){
     const distX = coordXprocent - positsonX
@@ -31,11 +50,12 @@ function setMouseParallaxStyle(){
     clouds.style.cssText = `transform: translate(${positsonX / forClouds}%,${positsonY / forClouds}%)`
     mountains.style.cssText = `transform: translate(${positsonX / forMountains}%,${positsonY / forMountains}%)`
     human.style.cssText = `transform: translate(${positsonX / forHuman}%,${positsonY / forHuman}%)`
-requestAnimationFrame(setMouseParallaxStyle)
+    requestAnimationFrame(setMouseParallaxStyle)
 
 }
-setMouseParallaxStyle()
-parallax.addEventListener("mousemove", function(e){
+
+
+function hadleMouseMove(e){
     const parallaxWidth = parallax.offsetWidth;
     const parallaxHeight = parallax.offsetHeight;
 
@@ -44,54 +64,18 @@ const coordY = e.pageY - parallaxHeight/2;
 
 coordXprocent = coordX / parallaxWidth * 100;
 coordYprocent = coordY / parallaxHeight * 100;
-})
-
-
-
-
-let thresholdSets = [];
-for (let i = 0; i <=1; i+=0.005) {
-    thresholdSets.push(i)
-    
 }
 
- const callback = function(){
+
+
+
+
+function setParallaxItemsStyle(){
     const scrollTopProcent = window.scrollY / parallax.offsetHeight * 100;
-    setParallaxItemsStyle(scrollTopProcent)
-}
 
-const observer = new IntersectionObserver(callback, {
-    threshold: thresholdSets
-});
-
-observer.observe(document.querySelector('.content'));
-
-function setParallaxItemsStyle(scrollTopProcent){
     content.style.cssText = `transform: translate(0%,-${scrollTopProcent / 9}%);`;
     mountains.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 6}%);`;
     human.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 3}%);`;
 }
 }
-}
-
-// parallax.addEventListener('mousemove', hadleMouse)
-
-
-// let parallaxWidth, parallaxHeigh;
-
-
-// function hadleMouse(e){
-// console.log(e);
-// }
-
-
-
-
-
-
-
-
-
-
-
 
